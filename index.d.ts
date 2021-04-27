01d8a5ce9f88451ec753ef475d76a846620aff12
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2021-04-27 05:03:25 +0100

    added a doc

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,6 @@
-declare function typeOf(value: any): any;
+/**
+ * Retrieves the type of a JS value
+ * @param {any} value the value to check its type
+ */
+declare function typeOf(value: any): string;
 export { typeOf as default };
--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-export default function typeOf(value: any) {
+/**
+ * Retrieves the type of a JS value
+ * @param {any} value the value to check its type
+ */
+export default function typeOf(value: any): string {
   if (typeof value !== "object") return typeof value;
   return !value ? String(value) : value.constructor.name.toLowerCase();
 }
